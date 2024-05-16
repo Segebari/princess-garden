91053f69e8b9f61a3ff25a709584160d5b3646fc
Segebari <41949716+Segebari@users.noreply.github.com>
2024-05-16 11:54:29 +0100

    cc

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Menu from './Menu'
 import { HashLink as Link } from 'react-router-hash-link'
-import logo from '../assets/logo.png'
+// import logo from '../assets/logo.png'
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -18,7 +18,8 @@ const Nav = () => {
     <>
       <div className="w-auto section-small-screens h-auto flex lg:flex-row justify-between py-7 text-lg section bg-topbg">
         <Link to="/">
-        <img src={logo} alt="logo" className='w-12' />
+          PG
+        {/* <img src={logo} alt="logo" className='w-12' /> */}
         </Link>
         <div
           className={`hidden  lg:flex lg:gap-10 lg:items-center xs:absolute lg:relative bg-topbg xs:h-[100vh] lg:h-[auto] xs:w-[100vw] lg:w-[auto] xs:text-center lg:text-left xs:left-0 lg:left-[auto] z-40 ${
@@ -27,7 +28,7 @@ const Nav = () => {
         >
           <Menu closeMenu={closeMenu} /> {/* Pass closeMenu function as prop */}
           <button className="border px-6 py-2 hover:bg-secondary hover:border-secondary hover:text-topbg xs:mt-10 lg:mt-0 xs:w-[40%] lg:w-[auto]">
-            <a href="tel:+2347034842501">Book Now</a>
+            <a href="tel:+2348148260644">Book Now</a>
           </button>
         </div>
 
